cd43b68248ebfaf98ca67bca361f126a4c27e703
lucekdudek <lucekdudek@gmail.com>
2015-12-15 20:55:09 +0100

    autoryzacja

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-from core.views import index, space
+from core.views import index, space, signout, signin
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'(?P<space_id>[0-9a-z-]+)/$', space),
+    url(r'^$', index, name="index"),
+    url(r'^in/$', signin, name="signin"),
+    url(r'^out/$', signout, name="signout"),
+    url(r'(?P<space_id>[0-9a-z-]+)/$', space, name="space"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,45 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import  render, get_object_or_404
-from core.models import Space
+from core.models import Space, SpaceUser
+from django.contrib.auth import authenticate, login, logout
 
+def user_is_space_user(user, space):
+    return SpaceUser.objects.get(space=space) in SpaceUser.objects.filter(user=user)
+
+@login_required(login_url='/out/')
 def index(request):
-    return render(request, 'index.html')
+    space_users_list = SpaceUser.objects.filter(user=request.user)
+    space_list = [su.space for su in space_users_list]
+    context = {'space_list': space_list}
+    return render(request, 'index.html', context)
 
 def space(request, space_id):
-    spaces_list = Space.objects.all()
     space = get_object_or_404(Space, pk=space_id)
-    context = {'spaces_list': spaces_list, 'space': space}
-    return render(request, 'space.html', context)
+    if user_is_space_user(request.user, space):
+        context = {'space': space}
+        return render(request, 'space.html', context)
+    else:
+        return index(request)
+
+def signin(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return index(request)
+        else:
+            errormessage = "Użytkownik jest nieaktywny."
+            context = {'errormessage': errormessage}
+            return render(request, 'signin.html', context)
+    else:
+        errormessage = "Wystąpił błąd autoryzacji."
+        context = {'errormessage': errormessage}
+        return render(request, 'signin.html', context)
+
+def signout(request):
+    logout(request)
+    return render(request, 'signin.html')
