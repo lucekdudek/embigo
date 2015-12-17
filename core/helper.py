f6a6a5c068e04209f9a808802c22e62f531db23b
lucekdudek <lucekdudek@gmail.com>
2015-12-17 12:10:19 +0100

    docstrings

--- core/helper.py
+++ core/helper.py
@@ -2,15 +2,29 @@
 from core.models import Space, SpaceUser
 
 def embigo_default_rights():
+    """
+    :return: rights of brand new user
+    """
     return "010000000000"
 
 def owner_default_rights():
+    """
+    :return: rights of creator of space
+    """
     return "111111111111"
 
 def embigo_main_space():
+    """
+    :return: star space
+    """
     return Space.objects.get(uid="00000000-0000-0000-0000-000000000000")
 
 def user_is_space_user(user, space):
+    """
+    :param user: get user
+    :param space: get space
+    :return: True if SpaceUser exist, else: False
+    """
     try:
         su = SpaceUser.objects.get(space=space, user=user)
     except SpaceUser.DoesNotExist:
@@ -20,4 +34,10 @@ def user_is_space_user(user, space):
     return True if 'su' in locals() else False
 
 def get_space_user(user, space):
+    """
+    note that this funcion can rasie error: paceUser.DoesNotExist
+    :param user: user
+    :param space: space
+    :return: SpaceUser
+    """
     return SpaceUser.objects.get(space=space, user=user)
