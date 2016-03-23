3cb0809f68dc03e26575c5d042f7f74d1e52017e
Michal <michall1100@gmail.com>
2016-03-23 18:00:19 +0100

    Fixed encrypt and decrypt functions.

--- core/crypto.py
+++ core/crypto.py
@@ -6,7 +6,7 @@ SECRET_KEY_WEBSOCKET = "password"
 def encrypt(key, text):
     tab = bytearray(len(text))
     for i in range(len(text)):
-        number = ord(text[i]) ^ ord(key[i%len(key)])
+        number = (ord(text[i])+1) ^ ord(key[i%len(key)])
         tab[i] = number
     return base64.b64encode(tab)
 
@@ -16,7 +16,7 @@ def decrypt(key, text):
     value = ""
     for i in range(len(tab)):
         number = tab[i] ^ ord(key[i%len(key)])
-        value += chr(number)
+        value += chr(number-1)
     return value
 
 def decode_utf_8(data):
